55370d67fd140e6dcf155a5f9f1116a62f269b56
yanntchoufack <yanntchoufack@gmail.com>
2025-11-12 15:22:25 +0100

    Update opérateurs.c

---
+++ TP1/src/opérateurs.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    int a = 16, b = 3;
+
+    printf("a + b = %d\n", a + b);
+    printf("a - b = %d\n", a - b);
+    printf("a * b = %d\n", a * b);
+    printf("a / b = %d\n", a / b);
+    printf("a %% b = %d\n", a % b);
+
+    printf("a == b ? %d\n", a == b);
+    printf("a > b  ? %d\n", a > b);
+
+    return 0;
+}
