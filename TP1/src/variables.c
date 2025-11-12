017ada58d06ec2d826281241c058599f51005262
yanntchoufack <yanntchoufack@gmail.com>
2025-11-12 15:04:29 +0100

    Update variables.c

---
+++ TP1/src/variables.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    // Déclaration et affectation des variables
+
+    // Type char
+    signed char c1 = -100;
+    unsigned char c2 = 200;
+
+    // Type short
+    signed short s1
